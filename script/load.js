c1bbbf666ee8a04eee66318cd42da9569918a89f
unknown <phanquang180918@gmail.com>
2024-05-25 12:32:47 +0700

    load

--- script/load.js
+++ script/load.js
@@ -1,7 +1,11 @@
-function showLoadingSpinner() {
-    document.querySelector('.spinner-overlay').style.display = 'block';
-    setTimeout(function () {
-      document.querySelector('.spinner-overlay').style.display = 'none';
-    }, 1000);
-  }
-  window.onload =showLoadingSpinner();
+function logoLoading() {
+  document.querySelector('.logo-overlay').style.display = 'block';
+  var img = document.querySelector('.logo-overlay img');
+  var src = img.src;
+  img.src = '';
+  img.src = src;
+  setTimeout(function () {
+      document.querySelector('.logo-overlay').style.display = 'none';
+  }, 2500);
+}
+window.onload = logoLoading();
